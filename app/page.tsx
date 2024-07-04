a1d1f1787dacbd936b8d33ae3bd10cc84bc662e5
tam <sarawut_bm@outlook.com>
2024-07-04 20:41:07 +0700

    updated responsive

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,17 @@ export default function Home() {
   const [searchCompany, setSearchCompany] = useState<string>('')
 
   return (
-    <div className='space-y-[2rem]'>
-      <div className='flex justify-between items-center'>
-        <div className='flex items-center space-x-4'>
-          <ImBooks className='w-10 h-10' />
-          <h2 className='font-bold text-[1.8rem]'>ระบบจัดการข้อมูลบัญชีบริษัท</h2>
+    <div className='xl:space-y-[2rem] space-y-[1rem]'>
+      <div className='xl:flex xl:justify-between xl:items-center xl:space-y-0 space-y-4'>
+        <div className='flex items-center lg:space-x-4 space-x-2'>
+          <ImBooks className='lg:w-10 lg:h-10 w-8 h-8' />
+          <h2 className='lg:text-[1.8rem] text-[1.3rem] font-bold'>ระบบจัดการข้อมูลบัญชีบริษัท</h2>
         </div>
-        <div className='flex items-center space-x-4'>
-          <Input type='number' onChange={(e) => setSearchCompany(e.target.value)} className='text-[1rem] w-[30rem] h-[2.6rem] rounded-xl' placeholder="ค้นหาสถานประกอบการด้วยเลขประจำตัวผู้เสียภาษี..." allowClear />
+        <div className='xl:flex xl:items-center xl:space-x-4 xl:space-y-0 space-y-4'>
+          <Input type='number' onChange={(e) => setSearchCompany(e.target.value)} className='xl:w-[30rem] text-[1rem] h-[2.6rem] rounded-xl' placeholder="ค้นหาสถานประกอบการด้วยเลขประจำตัวผู้เสียภาษี..." allowClear />
           <Link href={'/company/add'} className='h-full flex justify-center items-center space-x-2 p-2 bg-green-600 text-white hover:bg-white hover:text-green-600 rounded-xl hover:scale-105 duration-300'>
             <IoIosAddCircle className='w-6 h-6' />
-            <span>เพิ่มข้อมูล</span>
+            <span className='text-[1rem]'>เพิ่มข้อมูล</span>
           </Link>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       screens: {
-        sm: "640px",
+        sm: "425px",
         md: "768px",
         lg: "1024px",
         xl: "1280px",
-        "2xl": "1536px",
+        "2xl": "1440px",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
