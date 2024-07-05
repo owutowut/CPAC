297fe7be8e03ef7fa88f078a961141ce53e21769
tam <sarawut_bm@outlook.com>
2024-07-05 18:57:33 +0700

    improve login form

--- components/form/LoginForm.tsx
+++ components/form/LoginForm.tsx
@@ -1,8 +1,7 @@
-// components/LoginModal.js (or components/LoginModal.tsx)
-
 import { useState } from 'react';
 import { Modal, Form, Input, Button, Alert } from 'antd';
 import { initialLoginError, initialLoginSuccess } from '@/utils/initial'
+import { useRouter } from 'next/navigation';
 
 // Define PropTypes for the component
 interface LoginModalProps {
@@ -15,6 +14,8 @@ const LoginModal: React.FC<LoginModalProps> = ({ visible, onClose }) => {
   const [showAlert, setShowAlert] = useState<boolean>(false);
   const [alert, setAlert] = useState<any>(initialLoginSuccess);
 
+  const router = useRouter()
+
   const handleLogin = () => {
     if (password === process.env.NEXT_PUBLIC_WEBSITEGATE) {
       setAlert(initialLoginSuccess)
@@ -22,14 +23,15 @@ const LoginModal: React.FC<LoginModalProps> = ({ visible, onClose }) => {
       setTimeout(() => {
         setShowAlert(false)
         localStorage.setItem('accessACPN', 'true')
+        router.push('/')
         onClose();
       }, 1000);
     } else {
       setAlert(initialLoginError)
       setShowAlert(true)
       setTimeout(() => {
-        localStorage.setItem('accessACPN', 'false')
         setShowAlert(false)
+        router.push('/')
       }, 3000);
     }
   };
@@ -43,7 +45,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ visible, onClose }) => {
           Login
         </Button>,
       ]}
-      rootClassName='atnd-modal'
+      rootClassName='atnd-modal-login'
     >
       <Form layout="vertical">
         <Input.Password className='antd-login' value={password} onChange={(e) => setPassword(e.target.value)} />
